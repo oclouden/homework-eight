ac17f0b55872c426a04fef100ca030ffdb12c534
oclouden <loudenowen@gmail.com>
2023-02-22 23:11:20 -0500

    final commit

--- app/app.js
+++ app/app.js
@@ -1,11 +1,24 @@
+var homeContent = 
+'<h1>Home</h1> <p>Testing</p>';
+var aboutContent =
+'<h1>About</h1> <p>Testing</p> ';
+var  productsContent =
+'<h1>Products</h1> <p>Testing</p>';
+var contactContent =
+'<h1>Home</h1> <p>Testing</p>';
+
+
 function initListeners() {
-    $("nav a").click(function(){
+    $("nav a").click(function(e){
+
         let aID = e.currentTarget.id;
-        console.log(aID);
+        let contentID = aID + "Content";
+
+        $("#app").html(eval(contentID));
     });
 }
 
 
-$(document).ready(function(event) {
+$(document).ready(function(e) {
     initListeners();
 });
